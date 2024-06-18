40598c892fc0c2c1d63108827ac860f9e56508b2
xianfeng596 <77441157+xianfeng596@users.noreply.github.com>
2024-06-18 13:56:22 +0800

    Update ContentView.swift

--- kfd/ContentView.swift
+++ kfd/ContentView.swift
@@ -62,7 +62,7 @@ struct ContentView: View {
                             puaf_pages = 0
                             kfd = 0
                         }.disabled(kfd == 0).frame(minWidth: 0, maxWidth: .infinity)
-                    }.buttonStyle(.bordered)
+                    }
                 }.listRowBackground(Color.clear)
                 if kfd != 0 {
                     Section {
