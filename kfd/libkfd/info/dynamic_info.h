c4bda33f8526b12393c084e1f164a32e0b65fc40
xianfeng <77441157+xianfeng596@users.noreply.github.com>
2024-10-05 21:58:54 +0800

    Update dynamic_info.h

--- kfd/libkfd/info/dynamic_info.h
+++ kfd/libkfd/info/dynamic_info.h
@@ -61,7 +61,7 @@ struct dynamic_info {
 const struct dynamic_info kern_versions[] = {
     // iOS 15.7.6 - iPhone 6s
     {   
-        .kern_version = "Darwin Kernel Version 21.6.0: Mon Apr 24 22:03:46 PDT 2023; root:xnu-8020.241.30~1/ RELEASE_ARM64_S8000",
+        .kern_version = "Darwin Kernel Version 21.6.0: Mon Apr 24 22:03:46 PDT 2023; root:xnu-8020.241.30~1/RELEASE_ARM64_S8000",
         .kread_kqueue_workloop_ctl_supported = false,
         .krkw_iosurface_supported = true,
         .perf_supported = true,
